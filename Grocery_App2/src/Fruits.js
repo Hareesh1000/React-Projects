3a0f6a2d222485ae2637c8c8c82d51f6f275c64d
Hareesh <hareeshraj10@gmail.com>
2025-07-27 21:57:40 +0530

    GroceryApp2 changes

--- Grocery_App2/src/Fruits.js
+++ Grocery_App2/src/Fruits.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductFruits from './data/ProductFruits.json'
 import Filters from './Filters'
 
@@ -7,8 +7,50 @@ import FruitCategories from './data/FruitsCategories.json'
 
 function Fruits() {
 
-     {/* Display only first five items in the Home page ------*/}
+    const [addBtnState, setBtnState] = useState([]);
 
+    const [changeCount, setChangeCount] = useState(0);
+     
+
+
+    const showitemCountButton = (itemName) => {
+      setBtnState(
+        (prevState) => { return [...prevState,itemName]}
+      )
+
+       setChangeCount(changeCount +1);
+      
+    }
+
+    const addRemoveItem = (itemState,itemName) => {
+
+      if (itemState =='INCREMENT'){
+        setChangeCount(changeCount +1);
+      }
+
+      else if (itemState == 'DECREMENT') {
+        setChangeCount(changeCount-1);
+      }
+
+    }
+    // useEffect(  
+    //   ()=>{
+    //     if (setChangeCount ==0) {
+    //        setChangeCount(changeCount +1);
+    //     }
+    //   },[setChangeCount]
+    // )
+
+//     const showitemCountButton = (item) => {
+//   const add_buttons = document.getElementById(item);
+//       console.log(item)
+//       setBtnState('Added')
+//       console.log(addBtnState);
+//     //  add_buttons.style.display = 'none';
+
+// }
+
+    
      const showProductFruits = ProductFruits.map (
     (item) =>(
 <div className='ProductCard'>
@@ -27,7 +69,15 @@ function Fruits() {
                  <div className='addSection'>
                   
                   <i class="fa-regular fa-bookmark"></i>
-                  <button className='add_button'>Add</button>
+                  {addBtnState.includes(item.name) && changeCount >0 ? <div className='itemCountButton'> 
+                                          <button onClick={()=>{addRemoveItem('DECREMENT',item.name)}}> -</button>
+                                          <p>{changeCount}</p>
+                                           <button onClick={()=>{addRemoveItem('INCREMENT',item.name)}}> +</button>
+                                        </div>
+                                        :   
+                  <button className='add_button' id={item.name} onClick={()=>{showitemCountButton(item.name)}}>Add</button>
+
+}
                  </div>
                   
                </div>
