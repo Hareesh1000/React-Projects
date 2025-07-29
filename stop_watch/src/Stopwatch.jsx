37e6c06d9440c4bc8c6b5c8333864d71107fb265
Hareesh <hareeshraj10@gmail.com>
2025-07-29 23:20:09 +0530

    Stopwatch update

--- stop_watch/src/Stopwatch.jsx
+++ stop_watch/src/Stopwatch.jsx
@@ -1,16 +1,54 @@
-import React from 'react'
+import React, { useState,useRef } from 'react'
 
 function Stopwatch() {
+
+    const[time,setTime] = useState(0);
+    const[runStatus,setRunStatus] = useState(null);
+     const intervalId = useRef(null);
+
+    let hh='00';
+    let mm='00';
+    let ss='00';
+
+    const startWatch= ()=>{
+        if(runStatus) return; 
+         intervalId.current = setInterval(
+                ()=>{
+                    setTime(prevtime => prevtime +1)
+                },1000
+            );
+
+        setRunStatus(true);
+    }
+
+    const stopWatch=()=>{
+        setRunStatus(null);
+        setTime(0);
+        if ( intervalId.current){
+                clearInterval(intervalId.current);  
+                intervalId.current = null;
+        }
+        
+    }
+
+    const pauseWatch=()=>{
+          if ( intervalId.current){
+                clearInterval(intervalId.current);  
+                intervalId.current = null;
+        }    
+        setRunStatus(null);  
+    }
+
   return (
     <div className='main'>
         <div className='stopWatch'>
             <div className='stopwatchDisplay'>
-                <h2>00:00:00:000</h2>
+                <h2>{hh}:{mm}:{time}</h2>
             </div>
             <div className='buttons'>
-                <button id='btnStart'>Start</button>
-                <button id='btnStop'>Stop</button>
-                <button id='btnPause'>Pause</button>
+                <button id='btnStart' onClick={startWatch}>Start</button>
+                <button id='btnStop' onClick={stopWatch}>Stop</button>
+                <button id='btnPause' onClick={pauseWatch}>Pause</button>
             </div>
         </div>
     </div>
