024a02181023fdee498dff570e055ebc49d5555c
Hareesh <hareeshraj10@gmail.com>
2025-08-05 20:24:25 +0530

    Folder restructure

---
+++ gallery_app/client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import GalleryApp from './Gallery';
+
+function App() {
+  return (
+    <div className="App">
+      <GalleryApp></GalleryApp>
+    </div>
+  );
+}
+
+export default App;
